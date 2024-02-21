1f543770b76a42702bbb6511dd473ee08b4c1013
tjhind <tjhind9@sky.com>
2024-02-21 15:02:57 +0000

    add initial hard-coded user seating plan with first error handling

--- components/Customer-seating-selection.js
+++ components/Customer-seating-selection.js
@@ -1,12 +1,110 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import { Button } from '../helpers'
+import { View, Text } from 'react-native'
+import { Button, SeatButton, DisabledSeatButton } from '../helpers'
 import { styles } from '../style-sheet'
+import { seatStyles } from '../style-sheet-seats.js'
+import { useState } from 'react'
 
 function CustomerSeating({ navigation }) {
+  const availableSeats = ['A1', 'B1', 'B2', 'B3'] // GET /api/events/:event_id (available_seats selected by business user)
+  const [selectedSeats, setSelectedSeats] = useState([]) // PATCH /api/auctions/:event_id (seat_selection)
+  const auctionSeats = ['A1', 'B2', 'B3'] // GET /api/auctions/:event_id (seat_selection)
+  const seatingPlan = [
+    ['A1', 'A2', 'A3', 'A4'],
+    ['B1', 'B2', 'B3', 'B4'],
+    ['C1', 'C2', 'C3', 'C4'],
+  ] // GET /api/businesses (seating_layout)
+  const currentPrice = '£2' //GET /auctions/:auctions/event/:event_id
+  const [selectionIsAuction, setSelectionIsAuction] = useState('false') //set to true when user selects seat in auction
+  const [selectionIsAvailable, setSelectionIsAvailable] = useState('true') //set to false when user selects seat in auction
+  const auctionSelection = []
+  const availableSelection = []
   return (
     <View style={styles.container}>
+      <View style={seatStyles.seatsContainer}>
+        {seatingPlan.map((row, i) => {
+          return (
+            <>
+              <View key={i} style={seatStyles.rowContainer}>
+                {row.map((seat) => {
+                  const isAvailable = availableSeats.includes(seat)
+                  const isAuctioning = auctionSeats.includes(seat)
+                  const isSelected = selectedSeats.includes(seat)
+                  return (
+                    <>
+                      {isAvailable && !isAuctioning ? (
+                        <SeatButton
+                          seatStyle={
+                            isSelected
+                              ? seatStyles.selectedSeatButton
+                              : seatStyles.availableSeatButton
+                          }
+                          key={seat}
+                          btnText={currentPrice}
+                          onPress={() => {
+                            {
+                              isSelected
+                                ? setSelectedSeats(
+                                    selectedSeats.filter(
+                                      (item) => seat !== item
+                                    )
+                                  )
+                                : setSelectedSeats([...selectedSeats, seat])
+                            }
+                          }}
+                        ></SeatButton>
+                      ) : isAuctioning ? (
+                        <SeatButton
+                          seatStyle={
+                            isSelected
+                              ? [seatStyles.selectedAuctionSeatButton]
+                              : seatStyles.auctionSeatButton
+                          }
+                          key={seat}
+                          btnText={currentPrice}
+                          onPress={() => {
+                            {
+                              isSelected
+                                ? setSelectedSeats(
+                                    selectedSeats.filter(
+                                      (item) => seat !== item
+                                    )
+                                  )
+                                : setSelectedSeats([...selectedSeats, seat])
+                            }
+                          }}
+                        ></SeatButton>
+                      ) : (
+                        <DisabledSeatButton
+                          disabled={true}
+                          seatStyle={seatStyles.unavailableSeatButton}
+                          key={seat}
+                          btnText={'     '}
+                        ></DisabledSeatButton>
+                      )}
+                    </>
+                  )
+                })}
+              </View>
+            </>
+          )
+        })}
+      </View>
+      {selectedSeats.forEach((selectedSeat) => {
+        const isAuctioning = auctionSeats.includes(selectedSeat)
+        isAuctioning
+          ? auctionSelection.push(selectedSeat)
+          : availableSelection.push(selectedSeat)
+      })}
+      {auctionSelection.length && availableSelection.length ? (
+        <View style={seatStyles.errorContainer}>
+          <Text style={seatStyles.textbox}>
+            You cannot select tickets both in auction and not in auction.
+          </Text>
+        </View>
+      ) : null}
       <Button
+        key={'auctionButton'}
         btnText="View Auction"
         onPress={() => navigation.navigate('AuctionPage')}
       />
