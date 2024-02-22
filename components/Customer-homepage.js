d06bfa90119b426776b66ac2cad4eab420909a0d
bensuth86 <bensuth86@gmail.com>
2024-02-22 13:56:06 +0000

    events layout

--- components/Customer-homepage.js
+++ components/Customer-homepage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom'
 import { View, ScrollView, Text, TouchableOpacity, StyleSheet, ActivityIndicator } from "react-native";
