a9c76634a9264ebee2f1fcc09b64223548b111c6
tjhind <tjhind9@sky.com>
2024-02-24 19:04:24 +0000

    add date and time picker for screening

--- components/Business-create-new-screening.js
+++ components/Business-create-new-screening.js
@@ -1,17 +1,18 @@
-import { generateSeatGrid } from '../helpers'
 import { useState } from 'react'
 import {
   ActivityIndicator,
-  View,
-  Text,
   TouchableOpacity,
   ScrollView,
   TextInput,
-} from 'react-native-web'
+  Text,
+  View,
+} from 'react-native'
+import DateTimePicker from '@react-native-community/datetimepicker'
 import { SeatButton, Button } from '../helpers'
 import { styles } from '../style-sheet'
 import { seatStyles } from '../style-sheet-seats.js'
 import { homeStyles } from '../style-sheet-customer-home.js'
+
 //accepts user input from signup
 // const [availableSeats, setAvailableSeats] = useState([]) // PATCH /api/events/event_id (available_seats)
 function BusinessCreateScreening() {
@@ -24,8 +25,20 @@ function BusinessCreateScreening() {
   const [selectedSeats, setSelectedSeats] = useState([])
   const [loading, setIsLoading] = useState(false)
   const [price, setPrice] = useState(1)
+  const [date, setDate] = useState(new Date())
+  const [show, setShow] = useState(false)
+  const [mode, setMode] = useState('date')
 
   //patch with changePrice
+
+  function onChange(e, selectedDate) {
+    setDate(selectedDate)
+    setShow(false)
+  }
+  function showMode(modeToShow) {
+    setShow(true)
+    setMode(modeToShow)
+  }
 
   function increasePrice() {
     setPrice((prevPrice) => prevPrice + 1)
@@ -49,11 +62,19 @@ function BusinessCreateScreening() {
       <View style={styles.container}>
         <View style={{ maxWidth: 300 }}></View>
         <View style={{ marginTop: 20, marginBottom: 20 }}>
+          <Button onPress={() => showMode('date')} btnText={"Choose Date"} />
+          <Button onPress={() => showMode('time')} btnText={"Choose Time"} />
+          {show && (
+            <DateTimePicker
+              value={date}
+              mode={mode}
+              is24HOUR={true}
+              onChange={onChange}
+            />
+          )}
           <View style={homeStyles.radiusSelection}>
-            <Text style={{ textAlign: 'center', marginBottom: 10 }}>
-              Please select your starting price:
-            </Text>
-            <Button btnText={'-'} onPress={decreasePrice} />
+            <Text>Please select your starting price:</Text>
+            <Button btnText={"-"} onPress={decreasePrice} />
             <TextInput
               value={price.toString()}
               onChangeText={(text) =>
@@ -62,95 +83,96 @@ function BusinessCreateScreening() {
               keyboardType="numeric"
               style={homeStyles.numberDial}
             />
-            <Button btnText={'+'} onPress={increasePrice} />
+            <Button btnText={"+"} onPress={increasePrice} />
           </View>
 
-          <View style={seatStyles.rowContainer}>
-            <Text
-              style={{ textAlign: 'center', marginTop: 20, marginBottom: 20 }}
-            >
-              Please select all available seats:
-            </Text>
-            {seatingPlan.map((row, i) => {
-              return (
-                <View key={i}>
-                  <View key={i} style={seatStyles.rowContainer}>
-                    {row.map((seat) => {
-                      const isSelected = selectedSeats.includes(seat)
-                      return (
-                        <View key={seat}>
-                          <SeatButton
-                            seatStyle={
-                              isSelected
-                                ? seatStyles.availableSeatButton
-                                : seatStyles.numberedSeatButton
-                            }
-                            key={seat}
-                            btnText={seat}
-                            onPress={() => {
-                              {
-                                isSelected
-                                  ? setSelectedSeats(
-                                      selectedSeats.filter(
-                                        (item) => seat !== item
-                                      )
-                                    )
-                                  : setSelectedSeats([...selectedSeats, seat])
-                              }
-                            }}
-                          ></SeatButton>
-                        </View>
-                      )
-                    })}
-                  </View>
-                </View>
-              )
-            })}
-          </View>
-          <View style={{ marginTop: 10 }}>
-            <View style={seatStyles.keyContainer}>
-              <View
-                style={[seatStyles.seatKey, { backgroundColor: '#7bc47f' }]}
-              ></View>
-              <Text>Available</Text>
-            </View>
-
-            <View style={seatStyles.keyContainer}>
-              <View
-                style={[seatStyles.seatKey, { backgroundColor: '#626262' }]}
-              ></View>
-              <Text>Unavailable</Text>
-            </View>
-          </View>
-          <TouchableOpacity
-            onPress={() => {
-              Alert.alert(
-                'Selection instructions',
-                'This is where all of the info for seat selections will be.'
-              )
-            }}
-            title="?"
+          <Text
+            style={{ textAlign: 'center', marginTop: 20, marginBottom: 20 }}
           >
-            <Text>?</Text>
-          </TouchableOpacity>
-          {selectedSeats.length > 0 ? (
-            <Button
-              key={'listeventbtn'}
-              btnText="List event"
-              onPress={() =>
-                navigation.navigate('BusinessHomepage', {
-                  business_id: { business_id },
-                })
-              }
-            />
-          ) : (
-            <View style={seatStyles.errorContainer}>
-              <Text style={seatStyles.textbox}>
-                You must select at least one seat.
-              </Text>
-            </View>
-          )}
+            Please select all available seats:
+          </Text>
+          {seatingPlan.map((row, i) => {
+            return (
+              <View key={i} style={seatStyles.seatsContainer}>
+                <View key={i} style={seatStyles.rowContainer}>
+                  {row.map((seat) => {
+                    const isSelected = selectedSeats.includes(seat)
+                    return (
+                      <View key={seat}>
+                        <SeatButton
+                          seatStyle={
+                            isSelected
+                              ? seatStyles.availableSeatButton
+                              : seatStyles.numberedSeatButton
+                          }
+                          key={seat}
+                          btnText={seat}
+                          onPress={() => {
+                            {
+                              isSelected
+                                ? setSelectedSeats(
+                                    selectedSeats.filter(
+                                      (item) => seat !== item
+                                    )
+                                  )
+                                : setSelectedSeats([...selectedSeats, seat])
+                            }
+                          }}
+                        ></SeatButton>
+                      </View>
+                    )
+                  })}
+                </View>
+              </View>
+            )
+          })}
         </View>
+        <View style={seatStyles.screen}>
+          <Text style={seatStyles.screenText}>SCREEN</Text>
+        </View>
+        <View style={{ marginTop: 10 }}>
+          <View style={seatStyles.keyContainer}>
+            <View
+              style={[seatStyles.seatKey, { backgroundColor: '#7bc47f' }]}
+            ></View>
+            <Text>Available</Text>
+          </View>
+
+          <View style={seatStyles.keyContainer}>
+            <View
+              style={[seatStyles.seatKey, { backgroundColor: '#626262' }]}
+            ></View>
+            <Text>Unavailable</Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            Alert.alert(
+              'Selection instructions',
+              'This is where all of the info for seat selections will be.'
+            )
+          }}
+          title="?"
+        >
+          <Text>?</Text>
+        </TouchableOpacity>
+        {selectedSeats.length > 0 ? (
+          <Button
+            key={'listeventbtn'}
+            btnText="List event"
+            onPress={() =>
+              navigation.navigate('BusinessHomepage', {
+                business_id: { business_id },
+              })
+            }
+          />
+        ) : (
+          <View style={seatStyles.errorContainer}>
+            <Text style={seatStyles.textbox}>
+              You must select at least one seat.
+            </Text>
+          </View>
+        )}
       </View>
     </ScrollView>
   )
