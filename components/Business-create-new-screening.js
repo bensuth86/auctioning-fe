b7ca433ce013f17eac4f36f7d4b9c402041f4069
tjhind <tjhind9@sky.com>
2024-02-26 15:07:18 +0000

    add ability to list new screening

--- components/Business-create-new-screening.js
+++ components/Business-create-new-screening.js
@@ -8,26 +8,83 @@ import {
   Image,
   TouchableOpacity,
 } from 'react-native'
-import filter from 'lodash.filter'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { styles } from '../style-sheet'
 import { useRoute } from '@react-navigation/native'
 import { useEffect } from 'react'
 
-function BusinessCreateScreening({navigation}) {
+function BusinessCreateScreening({ navigation }) {
   const route = useRoute()
   const business_id = route.params.business_id
   const [loading, setIsLoading] = useState(false)
   const [searchQuery, setSearchQuery] = useState('')
   const [searchQuerySlug, setSearchQuerySlug] = useState('')
   const [data, setData] = useState([])
-  const [fullData, setFullData] = useState([])
+  const [idNo, setIdNo] = useState('')
   const [err, setErr] = useState(null)
-  const apiEndpoint = `https://www.omdbapi.com/?apikey=f593767e&t=${searchQuerySlug}`
   const [title, setTitle] = useState('')
   const [poster, setPoster] = useState('')
+  const [runtime, setRuntime] = useState('100')
+  const [certificate, setCertificate] = useState('12')
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetchData(movieEndpoint)
+  }, [searchQuery])
+
+  useEffect(() => {
+    fetchRuntime(idNo)
+  }, [idNo])
+
+  useEffect(() => {
+    fetchCertificate(idNo)
+  }, [idNo])
 
   const movieEndpoint = `https://api.themoviedb.org/3/search/movie?api_key=2ff74c9759be7b397da331e5c4e692ee&query=${searchQuerySlug}&include_adult=false&language=en-US&page=1`
+
+  const fetchRuntime = async (filmid) => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${filmid}?api_key=2ff74c9759be7b397da331e5c4e692ee`
+      )
+      const json = await response.json()
+      setIsLoading(false)
+      json.runtime < 40
+        ? setRuntime('100')
+        : setRuntime(json.runtime.toString())
+    } catch (error) {
+      setErr(error)
+      console.log(error)
+    }
+  }
+
+  const fetchCertificate = async (filmid) => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${filmid}/release_dates?api_key=2ff74c9759be7b397da331e5c4e692ee`
+      )
+      const json = await response.json()
+      const result = json.results
+      result.map((item) => {
+        if (item.iso_3166_1 === 'GB') {
+          if (item.release_dates.certification !== '') {
+            setCertificate(item.release_dates[0].certification)
+          }
+          if (
+            item.release_dates[0].certification === '' ||
+            item.release_dates[0].certification === null ||
+            item.release_dates[0].certification === undefined
+          ) {
+            setCertificate('12')
+          }
+        }
+      })
+      setIsLoading(false)
+    } catch (error) {
+      setErr(error)
+      console.log(error)
+    }
+  }
 
   function slugify(str) {
     return String(str)
@@ -40,11 +97,6 @@ function BusinessCreateScreening({navigation}) {
       .replace(/-+/g, '-')
   }
 
-  useEffect(() => {
-    setIsLoading(true)
-    fetchData(movieEndpoint)
-  }, [searchQuery])
-
   const fetchData = async (url) => {
     try {
       const response = await fetch(url)
@@ -55,6 +107,12 @@ function BusinessCreateScreening({navigation}) {
       setErr(error)
       console.log(error)
     }
+  }
+
+  function handlePress(item) {
+    setTitle(item.title)
+    setPoster(`https://image.tmdb.org/t/p/w500${item.poster_path}`)
+    setIdNo(item.id)
   }
 
   function handleSearch(query) {
@@ -98,15 +156,21 @@ function BusinessCreateScreening({navigation}) {
             renderItem={({ item }) => {
               return (
                 <View>
-                  <Image
-                    source={{
-                      uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                  <TouchableOpacity
+                    onPress={() => {
+                      handlePress(item)
                     }}
-                    style={{ width: 80, height: 100 }}
-                  />
-                  <View>
-                    <Text>{item.title}</Text>
-                  </View>
+                  >
+                    <Image
+                      source={{
+                        uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                      }}
+                      style={{ width: 80, height: 100 }}
+                    />
+                    <View>
+                      <Text>{item.title}</Text>
+                    </View>
+                  </TouchableOpacity>
                 </View>
               )
             }}
@@ -116,13 +180,14 @@ function BusinessCreateScreening({navigation}) {
       <TouchableOpacity
         style={styles.button}
         onPress={() => {
-            navigation.navigate('BusinessListingPage', { business_id })
-          }
-        }
-        // title: title,
-        // poster: poster,
-        // certificate: certificate,
-        // runtime: runtime,
+          navigation.navigate('BusinessListingPage', {
+            business_id,
+            title: title,
+            poster: poster,
+            runtime: runtime,
+            certificate: certificate,
+          })
+        }}
       >
         <Text style={styles.buttonText}>Continue</Text>
       </TouchableOpacity>
