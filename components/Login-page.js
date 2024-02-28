e61cce119ea4b4d2be75ac3bc6055783fc372b38
SoliferNN914 <oskarson16@gmail.com>
2024-02-28 15:17:23 +0000

    unique key prop error fix on previous order page

--- components/Login-page.js
+++ components/Login-page.js
@@ -12,9 +12,6 @@ import { TouchableOpacity } from 'react-native'
 import { ActivityIndicator } from 'react-native-paper'
 import { homeStyles } from '../style-sheet-customer-home'
 import { Pressable } from 'react-native'
-// import * as Device from 'expo-device';
-// import * as Notifications from 'expo-notifications';
-// import Constants from 'expo-constants';
 
 function Login({ navigation, route }) {
   const usertype = route.params.usertype
@@ -24,89 +21,8 @@ function Login({ navigation, route }) {
   const [submitBusinessClicked, setBusinessClicked] = useState(false)
   // const [match, setMatch] = useState(false)
   const [errorMessage, setErrorMessage] = useState('')
-  // const [expoPushToken, setExpoPushToken] = useState(null);
-  // const [notification, setNotification] = useState(false);
-  // const notificationListener = useRef();
-  // const responseListener = useRef();
 
   const { setCurrentCustomer } = useContext(CustomerContext)
-
-  //   Notifications.setNotificationHandler({
-  //     handleNotification: async () => ({
-  //       shouldShowAlert: true,
-  //       shouldPlaySound: false,
-  //       shouldSetBadge: false,
-  //     }),
-  //   });
-
-  //   async function sendPushNotification(expoPushToken) {
-  //     const message = {
-  //       to: expoPushToken,
-  //       title: 'Welcome Back!',
-  //       body: 'You have successfully logged in.',
-  //     };
-
-  //     await fetch('https://exp.host/--/api/v2/push/send', {
-  //       method: 'POST',
-  //       headers: {
-  //         Accept: 'application/json',
-  //         'Accept-encoding': 'gzip, deflate',
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(message),
-  //     });
-  //   }
-
-  // // CODE FROM EXPO DOCS
-  // async function registerForPushNotificationsAsync() {
-  //   let token;
-
-  //   if (Platform.OS === 'android') {
-  //     Notifications.setNotificationChannelAsync('default', {
-  //       name: 'default',
-  //       importance: Notifications.AndroidImportance.MAX,
-  //       vibrationPattern: [0, 250, 250, 250],
-  //       lightColor: '#FF231F7C',
-  //     });
-  //   }
-
-  //   if (Device.isDevice) {
-  //     const { status: existingStatus } = await Notifications.getPermissionsAsync();
-  //     let finalStatus = existingStatus;
-  //     if (existingStatus !== 'granted') {
-  //       const { status } = await Notifications.requestPermissionsAsync();
-  //       finalStatus = status;
-  //     }
-  //     if (finalStatus !== 'granted') {
-  //       alert('Failed to get push token for push notification!');
-  //       return;
-  //     }
-  //     token = await Notifications.getExpoPushTokenAsync({
-  //       projectId: Constants.expoConfig.extra.eas.projectId,
-  //     });
-  //   } else {
-  //     alert('Must use physical device for Push Notifications');
-  //   }
-
-  //   return token.data;
-  // }
-
-  //   useEffect(() => {
-  //     registerForPushNotificationsAsync().then(token => {console.log(token), setExpoPushToken(token)});
-
-  //     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-  //       setNotification(notification);
-  //     });
-
-  //     responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-  //       console.log(response);
-  //     });
-
-  //     return () => {
-  //       Notifications.removeNotificationSubscription(notificationListener.current);
-  //       Notifications.removeNotificationSubscription(responseListener.current);
-  //     };
-  //   }, []);
 
   useEffect(() => {
     if (submitCustomerClicked) {
@@ -116,7 +32,6 @@ function Login({ navigation, route }) {
         setLoading(false)
         response.data.users.forEach((user) => {
           if (loginName === user.username) {
-            // sendPushNotification(expoPushToken)
             setCurrentCustomer({
               username: user.username,
               user_id: user.user_id,
@@ -143,7 +58,6 @@ function Login({ navigation, route }) {
         setLoading(false)
         response.data.businesses.forEach((business) => {
           if (loginName === business.business_name) {
-            // sendPushNotification(expoPushToken)
             // setCurrentCustomer({ business: business.business_name });
             navigation.navigate('BusinessHomepage', {
               business_id: business.business_id,
